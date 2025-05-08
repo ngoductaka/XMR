b9140ef433b8f612483b45fb9f8115e5f1212d28
ducdn <ducdn@gmail.com>
2025-05-08 12:53:31 +0700

    update

--- create_chrome.js
+++ create_chrome.js
@@ -8,7 +8,9 @@ const {
     clickCMD,
     runRestartScript, } = require('./lib');
 
-const port = process.argv[2] ? parseInt(process.argv[2], 10) : 9225;
+const count = process.argv[2] ? parseInt(process.argv[2], 10) : 1;
+const port = parseInt((9220 + count), 10);
+const name = process.argv[3] ? process.argv[3] : 'm1_';
 
 // Validate port is a number and in valid range
 if (isNaN(port) || port < 1024 || port > 65535) {
@@ -21,36 +23,32 @@ const create = async (port, i) => {
     try {
         // openChrome(port);
         const page = await initConnection(port, i);
-        const classFound = await waitForClassToExist(page, '.is-loaded');
-        if (!classFound) {
-            console.log('Class not found, exiting...');
-            await browser.close();
-            return;
-        }
-        await clickCMD(page, i);
-        await page.keyboard.press('Enter');
-        console.log('Text typed into currently focused element');
+        await page.waitForSelector('iframe.is-loaded', { timeout: 10 * 60 * 1000 });
+        console.log('________done waiting for selector___________________________');
+        // const classFound = await waitForClassToExist(page, '.is-loaded');
+        // if (!classFound) {
+        //     console.log('Class not found, exiting...');
+        //     await browser.close();
+        //     return;
+        // }
+        // await clickCMD(page, i);
+        // await page.keyboard.press('Enter');
+        // console.log('Text typed into currently focused element');
 
     } catch (error) {
         console.error('Error killing Chrome:', error);
     }
 }
 
-const run10 = async(port) => {
+const run10 = async (port) => {
     for (let i = 0; i < 10; i++) {
-        await create(port, 'dnd'+i);
+        await create(port, name + i);
     }
 }
 
-console.log(`Opening Chrome with remote debugging on port ${port}`);
 const main = async () => {
-    openChrome(port)
-        .catch(err => {
-            openChrome(port)
-            setTimeout(() => {
-                run10(port);
-            }, 2000);
-        });
+    const profileName = `chrome-profile${port}`;
+    openChrome(port, profileName)
     setTimeout(() => {
         console.log('Chrome opened successfully');
         run10(port);
---
+++ run.js
@@ -0,0 +1,89 @@
+const {
+    openChrome,
+    openOldConnection,
+    resetWithLink,
+    killChromeProcess
+} = require('./lib');
+
+const count = process.argv[2] ? parseInt(process.argv[2], 10) : 1;
+const port = parseInt((9220 + count), 10);
+const name = process.argv[3] ? process.argv[3] : 'm1_';
+
+// Validate port is a number and in valid range
+if (isNaN(port) || port < 1024 || port > 65535) {
+    console.error('Please provide a valid port number between 1024 and 65535');
+    console.log('Usage: node openChrome.js [port]');
+    process.exit(1);
+}
+
+
+const reset = async (browser, link) => {
+    try {
+        const location = link.split('/');
+        const workerName = location[location.length - 1];
+        const page = await browser.newPage();
+
+        await page.goto(link);
+        await page.waitForSelector('iframe.is-loaded', { timeout: 8 * 60 * 1000 });
+
+        const iframeSrc = await page.evaluate(() => {
+            const iframe = document.querySelector('iframe.is-loaded');
+            return iframe ? iframe.src : null;
+        });
+        console.log('open workerName:', workerName);
+        await resetWithLink(page, iframeSrc, workerName).catch(async (err) => {
+            console.error('Error resetting with link::________________dnd____', err);
+            await resetWithLink(page, iframeSrc, workerName).catch()
+        });
+        console.log(workerName + 'done and closing page in 10 seconds');
+        setTimeout(async () => {
+            await page.close();
+            console.log(workerName + '_________closed___');
+        }, 10 * 1000);
+    } catch (error) {
+        console.error('Error killing Chrome:', error);
+    }
+}
+
+const create = async (page, name) => {
+    try {
+        await page.goto('https://idx.google.com/new/react-native');
+        await page.waitForSelector('#mat-input-0');
+        await page.type('#mat-input-0', name);
+        await page.keyboard.press('Enter');
+        await page.waitForSelector('iframe.is-loaded', { timeout: 10 * 60 * 1000 });
+        await page.close();
+    } catch (error) {
+        console.error('Error killing Chrome:', error);
+    }
+}
+
+const runJob = async (port) => {
+    const { browser, page, mainTargetLinks } = await openOldConnection(port);
+    if (mainTargetLinks.length < 10) {
+        for (let i = mainTargetLinks.length; i < 11; i++) {
+            await create(page, name + i).catch(() => create(page, name + i).catch());
+        }
+    }
+    mainTargetLinks.reverse();
+    for (const link of mainTargetLinks) {
+        await reset(browser, link.href).catch(() => reset(browser, link.href).catch());
+    }
+
+}
+const combineOpenReset = async (port) => {
+    const profileName = `chrome-profile${port}`;
+    await killChromeProcess(profileName)
+    openChrome(port, profileName)
+    setTimeout(() => {
+        console.log('Chrome opened successfully2');
+        runJob(port);
+    }, 2000);
+}
+const main = async () => {
+    combineOpenReset(port);
+    setInterval(() => {
+        combineOpenReset(port);
+    }, 1000 * 60 * 60);
+}
+main();
